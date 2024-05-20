c2d1bc702e7dcb7a981fd6736e07326f904d1cfc
selhatinenc <selhattinenc@hacettepe.edu.tr>
2024-05-20 22:01:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("layout", "layouts/main");
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    // we can set cokkie as cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }));
+
+app.locals.isActiveRoute = isActiveRoute; 
+
+// Router'ı içe aktarın
+const mainRouter = require("./server/routes/main");
+const mainRouter = require("./server/routes/admin");
+
+// Router'ı middleware olarak kullanın
+app.use("/", mainRouter);
+app.use("/", admin);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    }
+);
+
---
+++ server/routes/main.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+router.get("/", async (req, res) => {
+    try { 
+
+
+        const locals={
+            title:"My Blog",
+            description:"Welcome to my blog!",
+    
+        }
+        let perPage=10;
+        let page=req.query.page || 1;
+    
+        const Mydata= await Post.aggregate([{ $sort: { createdAt: -1 } }, { $skip: (perPage * page) - perPage }, { $limit: perPage }]);
+    
+        const count= await Post.countDocuments({});
+        const nextPage=parseInt(page)+1;
+        const CnextPage=nextPage<=Math.ceil(count/perPage);
+    
+    
+        res.render("index",{
+            locals,
+            data,
+            current:page,
+            nextPage:CnextPage? nextPage:null,
+            currentRoute:"/"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
+    });
+
+
+router.get("/post/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        const locals = {
+            title: data.title,
+            description: "Simple Blog created with NodeJs, Express & MongoDb.",
+          }
+        res.render('post', { 
+            locals,
+            data,
+            currentRoute: `/post/${slug}`
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.post("/search", async (req, res) => {
+    try {
+        const locals = {
+            title: "Seach",
+            description: "Simple Blog created with NodeJs, Express & MongoDb."
+            }
+        let searchtitle=req.body.searchTerm;
+        const searchNospecialChar=searchTerm.replace(/[^a-zA-Z0-9]/g, '');
+        const data = await Post.find({
+            $or: [
+              { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+              { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+            ]
+          });
+        res.render("search", {
+        data,
+        locals,
+        currentRoute: '/'
+        });
+      
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+      currentRoute: '/about'
+    });
+  });
+
+
+module.exports = router;
